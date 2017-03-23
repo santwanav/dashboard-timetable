26e23aaa79b2ce9568416649e1187478be3ad879
Yash Srivastav <admin@yashsriv.org>
2017-03-24 05:27:46 +0530

    Fix for working properly with FullCalendar

--- models.py
+++ models.py
@@ -1,14 +1,19 @@
+from uuid import uuid4
 from config import db
-from datetime import datetime
+
 
 class Event(db.Model):
     __tablename__ = 'event'
 
-    username = db.Column(db.String(20), primary_key = True)
-    description = db.Column(db.String(250), primary_key = True)
-    time = db.Column(db.DateTime, primary_key = True)
+    id = db.Column(db.String(32), primary_key=True)
+    username = db.Column(db.String(20))
+    description = db.Column(db.String(250))
+    start = db.Column(db.DateTime)
+    end = db.Column(db.DateTime)
 
-    def __init__(self, username, description, time):
+    def __init__(self, username, description, start, end):
+        self.id = uuid4().hex
         self.username = username
         self.description = description
-        self.time = time
+        self.start = start
+        self.end = end
--- server.py
+++ server.py
@@ -1,41 +1,54 @@
 #!/usr/bin/env python
 from flask import request, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from dateutil import parser
-from datetime import timedelta
 from config import app, rpc, db, SECRET, PORT
 from models import Event
+from sqlalchemy import or_
 
-def timetable_add(username, event):
-    parsed_time = parser.parse(event['time'])
-    newEvent = Event(username, event['name'], parsed_time)
+
+def timetable_add(params):
+    username = params['username']
+    event = params['event']
+    parsed_start = parser.parse(event['start'])
+    parsed_end = parser.parse(event['end'])
+    newEvent = Event(username, event['name'], parsed_start, parsed_end)
     db.session.add(newEvent)
     db.session.commit()
-    return {'successful' : newEvent != None}
+    return {'successful': newEvent is not None}
 
-def timetable_get(username, date):
-    new_date = parser.parse(date)
-    end_date = new_date + timedelta(days=7)
-    user = Event.query.filter_by(username = username)
-    time = user.filter(Event.time.between(str(new_date), str(end_date)))
+
+def timetable_get(params):
+    username = params['username']
+    start = params['start']
+    end = params['end']
+    new_date = parser.parse(start)
+    end_date = parser.parse(end)
+    user = Event.query.filter_by(username=username)
+    time = user.filter(or_(Event.start.between(str(new_date), str(end_date))),
+                       Event.end.between(str(new_date), str(end_date)))
     results = []
     for x in time.all():
         result = {}
-        result["username"] = x.username
+        result["id"] = x.id
         result["description"] = x.description
-        result["time"] = x.time.isoformat()
+        result["start"] = x.start.isoformat()
+        result["end"] = x.end.isoformat()
         results.append(result)
     return results
 
+
 @app.route('/', methods=["POST"])
 def handle():
-    return jsonify(rpc(request.json))
+    result = rpc(request.json)
+    return jsonify(result)
+
 
 @app.route('/create', methods=["POST"])
 def create():
     if request.json["secret"] == SECRET:
         db.create_all()
     return jsonify("Successful")
+
 
 if __name__ == "__main__":
     rpc['Timetable.add'] = timetable_add
